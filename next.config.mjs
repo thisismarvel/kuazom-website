2606e11624731b8b3c9f69da46226645b75adf12
Marvel <marvel@example.com>
2026-06-22 17:23:38 +0100

    Your update message here

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // <--- Add this line
+  // If you are using Next.js images, you might also need:
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
